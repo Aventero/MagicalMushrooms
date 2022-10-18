724b0edc8b8e48b93ded5110483cb9a19a81130c
Aquarelix <AquarelixOG@protonmail.com>
2022-10-18 14:11:42 +0200

    Added the new Input System

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Vector3 moveVal;
+    [SerializeField] private float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        this.transform.position += moveSpeed * Time.deltaTime * moveVal;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 inputVector = value.Get<Vector2>();
+
+        moveVal = new Vector3(inputVector.x, 0, inputVector.y);
+    }
+
+}
