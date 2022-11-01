be68144907fc685a3796944a81c954abc2bd43ec
Aquarelix <AquarelixOG@protonmail.com>
2022-11-01 17:31:20 +0100

    Improved Compassbar

--- Assets/Scripts/CompassBar.cs
+++ Assets/Scripts/CompassBar.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,29 +12,19 @@ public class CompassBar : MonoBehaviour
 
     private RectTransform compassTransform;
     private List<(GameObject, GameObject)> itemList;
-    private GameObject[] itemObjects;
+    private List<GameObject> itemObjects;
 
     void Start()
     {
         compassTransform = this.GetComponent<RectTransform>();
 
         itemList = new List<(GameObject, GameObject)>();
-        itemObjects = GameObject.FindGameObjectsWithTag("Item");
-        CreateIcons(itemObjects);
+        itemObjects = new List<GameObject>(GameObject.FindGameObjectsWithTag("Item"));
     }
 
     void Update()
     {
         UpdateIcons();
-    }
-
-    private void CreateIcons(GameObject[] itemGameObjects)
-    {
-        for(int i = 0; i < itemGameObjects.Length; i++)
-        {
-            GameObject newItemGameObject = CreateIcon(itemGameObjects[i]);
-            itemList.Add((itemGameObjects[i], newItemGameObject));
-        }
     }
 
     private GameObject CreateIcon(GameObject itemGameobject)
@@ -54,32 +46,48 @@ public class CompassBar : MonoBehaviour
 
     private void UpdateIcons()
     {
-        foreach((GameObject, GameObject) itemTuple in itemList)
+        foreach(GameObject itemObject in itemObjects)
         {
-            GameObject target = itemTuple.Item1;
-            GameObject spriteGameObject = itemTuple.Item2;
-            float distance = Vector3.Distance(target.transform.position, Camera.main.transform.position);
+            // Get the tuple with the item object
+            (GameObject, GameObject) currentTuple = itemList.Find(tuple => tuple.Item1 == itemObject);
 
-            if (target == null)
+            // Item has been destroyed
+            if (itemObject == null)
             {
-                Destroy(spriteGameObject);
-                itemList.Remove(itemTuple);
-                return;
-            }
-            else if( distance > MinDistance)
-            {
-                if (spriteGameObject != null)
-                    Destroy(spriteGameObject);
-                return;
-            }
-            else if(distance < MinDistance && spriteGameObject == null && target != null)
-            {
-                itemList.Remove(itemTuple);
-                itemList.Add((target, CreateIcon(target)));
+                // Remove sprite
+                if (currentTuple.Item2 != null)
+                    Destroy(currentTuple.Item2);
+
+                // Remove Item Object 
+                itemList.Remove(currentTuple);
+                itemObjects.Remove(itemObject);
                 return;
             }
 
-            UpdateMarker(target, spriteGameObject.GetComponent<RectTransform>());
+            // Calculate Distance from player to item
+            float distance = Vector3.Distance(Camera.main.transform.position, itemObject.transform.position);
+
+            if (distance < MinDistance)
+            {
+                if (currentTuple.Item1 == null)
+                {
+                    // Create Sprite if not already in list
+                    GameObject itemSprite = CreateIcon(itemObject);
+                    itemList.Add((itemObject, itemSprite));
+                }
+                else
+                {
+                    UpdateMarker(currentTuple.Item1, currentTuple.Item2.GetComponent<RectTransform>());
+                }
+            }
+            else
+            {
+                // Sprite is out of range -> Remove Sprite
+                if(currentTuple.Item2 != null)
+                    Destroy(currentTuple.Item2);
+
+                itemList.Remove(currentTuple);
+            }
         }
     }
 
