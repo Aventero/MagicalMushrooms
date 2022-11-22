24c8768dca58f40e3457d9436755d19d8c4d596f
Aquarelix <AquarelixOG@protonmail.com>
2022-11-22 18:54:15 +0100

    Added Interactable class

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    public float interactionSize = 2f;
+
+    public virtual void Start()
+    {
+        CreateSphereCollider();
+    }
+
+    protected virtual void CreateSphereCollider()
+    {
+        SphereCollider sphereCollider = this.gameObject.AddComponent<SphereCollider>();
+        sphereCollider.radius = interactionSize;
+        sphereCollider.isTrigger = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, interactionSize);
+    }
+}
