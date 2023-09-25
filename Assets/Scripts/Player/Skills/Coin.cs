7331dba9becf4365d9b2b3df62afb71179ca4d3f
Aquarelix <AquarelixOG@protonmail.com>
2023-09-25 17:51:30 +0200

    Added coin class

--- Assets/Scripts/Player/Skills/Coin.cs
+++ Assets/Scripts/Player/Skills/Coin.cs
@@ -2,8 +2,28 @@
 
 class Coin : MonoBehaviour
 {
+    public float travelSpeed;
+
+    private bool enableVacuum = false;
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");    
+    }
+
     public void ActivateVacuum()
     {
+        enableVacuum = true;
+    }
 
+    private void Update()
+    {
+        if (!enableVacuum)
+            return;
+
+        float percentage = 0;
+
+        Vector3.Lerp(this.transform.position, player.transform.position, percentage);
     }
 }
