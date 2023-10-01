891efd5065a28224b3ac8197b08a5377baf2d930
Aquarelix <AquarelixOG@protonmail.com>
2023-10-01 18:05:19 +0200

    Added Global Volume for the main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Continue()
+    {
+        NewGame();
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void Quit()
     {
         Debug.Log("Quitting the game! Bye!");
