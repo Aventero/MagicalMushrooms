6452d818cea5855dae1770dfa9b6404aa6252f04
Henrik <henrikweidner98@gmail.com>
2023-10-24 18:06:25 +0200

    Coin Vacuum actually uses radius

--- Assets/Scripts/Player/Skills/Coins/CoinVacuum.cs
+++ Assets/Scripts/Player/Skills/Coins/CoinVacuum.cs
@@ -6,12 +6,12 @@ public class CoinVacuum : MonoBehaviour
 {
     [Header("Vacuum Settings")]
     public Transform vacuumCenter;   // The point towards which coins will be dragged
-    public float vacuumRadius = 5f;  // The effective radius of the vacuum
+    public float vacuumSearchRadius = 5f;  // The effective radius of the vacuum
     public float vacuumForce = 5f;   // The force/speed at which coins are pulled towards the vacuum center
     private HashSet<Coin> activeCoins = new();
     public bool MouseHeld { get; private set; }
-    [Range(1, 10)] public float slurpRadius = 1f; // Start angle for the vacuum zone (relative to forward direction)
-    
+    [Range(0, 1)] public float slurpRadius = 1f; // Start angle for the vacuum zone (relative to forward direction)
+
     private GlassSlurpSpin glassSlurpSpin;
     private PlayerSkillManager skillManager;
 
@@ -71,21 +71,44 @@ public class CoinVacuum : MonoBehaviour
 
     public void Execute()
     {
-        // Find coins within the vacuum radius at the time of execution
-        Collider[] hitColliders = Physics.OverlapSphere(vacuumCenter.position, vacuumRadius);
-        foreach (Collider hitCollider in hitColliders)
-        {
+        //// Find coins within the vacuum radius at the time of execution
+        //Collider[] hitColliders = Physics.OverlapSphere(vacuumCenter.position, vacuumSearchRadius);
+        //foreach (Collider hitCollider in hitColliders)
+        //{
+        //    // Slurp
+        //    Coin coin = hitCollider.GetComponent<Coin>();
+        //    if (coin != null)
+        //    {
+        //        Vector3 toCoin = coin.transform.position - Camera.main.transform.position;
 
-            // Slurp
-            Coin coin = hitCollider.GetComponent<Coin>();
+        //        // Find the projection (or closest point) of the coin's position onto the line of the camera's forward direction
+        //        Vector3 projection = Camera.main.transform.position + Vector3.Dot(toCoin, Camera.main.transform.forward) * Camera.main.transform.forward;
+
+        //        // Compute the distance between the coin and the projection point
+        //        float distanceFromLine = Vector3.Distance(projection, coin.transform.position);
+
+        //        // Check if this distance is within the desired limit (cylinder's radius)
+        //        if (distanceFromLine <= vacuumSearchRadius)
+        //        {
+        //            activeCoins.Add(coin);
+        //        }
+        //    }
+        //}
+
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit[] hits;
+
+        float maxDistance = vacuumSearchRadius; // Length of the cylinder
+        float sphereRadius = slurpRadius; // Radius of the cylinder
+
+        hits = Physics.SphereCastAll(ray, sphereRadius, maxDistance);
+
+        foreach (RaycastHit hit in hits)
+        {
+            Coin coin = hit.collider.GetComponent<Coin>();
             if (coin != null)
             {
-                Vector3 toCoin = coin.transform.position - vacuumCenter.position;
-
-                if (IsWithinCylinder(toCoin, vacuumCenter.forward, slurpRadius))
-                {
-                    activeCoins.Add(coin);
-                }
+                activeCoins.Add(coin);
             }
         }
 
@@ -95,21 +118,19 @@ public class CoinVacuum : MonoBehaviour
         }
     }
 
-    bool IsWithinCylinder(Vector3 toTarget, Vector3 forwardDirection, float radius)
+    void OnDrawGizmos()
     {
-        // Check if the target is directly in front of the origin (in the cylinder)
-        RaycastHit hit;
-        if (Physics.Raycast(vacuumCenter.position, toTarget.normalized, out hit, radius))
-        {
-            if (hit.transform.GetComponent<Coin>() != null)
-            {
-                // Check if the target is within the cylindrical radius
-                Vector3 crossProduct = Vector3.Cross(toTarget.normalized, forwardDirection);
-                float distanceFromCenter = crossProduct.magnitude * toTarget.magnitude;
-                return distanceFromCenter <= radius;
-            }
-        }
-        return false;
-    }
+        Gizmos.color = Color.cyan;
 
+        // The start and end points of the sphere cast
+        Vector3 start = Camera.main.transform.position;
+        Vector3 end = start + Camera.main.transform.forward * vacuumSearchRadius;
+
+        // Draw the central line of the cylinder
+        Gizmos.DrawLine(start, end);
+
+        // Draw circles at the start and end to represent the sphere's path
+        Gizmos.DrawWireSphere(start, slurpRadius);
+        Gizmos.DrawWireSphere(end, slurpRadius);
+    }
 }
